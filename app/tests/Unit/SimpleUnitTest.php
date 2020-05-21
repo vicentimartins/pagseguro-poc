8ece6edf8e386419ca1b42973a7627725f979071
jhonatta Santos <jhonatta@cotacoesecompras.com.br>
2020-05-20 21:02:31 -0300

    Add phpunit as dependency

---
+++ app/tests/Unit/SimpleUnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleUnitTest extends TestCase
+{
+    public function testSimple()
+    {
+        $this->assertTrue(true);
+    }
+}
--- app/vendor/composer/autoload_real.php
+++ app/vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitd16eb6b572b74fc661029fcd5b2409d3
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -47,6 +50,24 @@ class ComposerAutoloaderInitd16eb6b572b74fc661029fcd5b2409d3
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitd16eb6b572b74fc661029fcd5b2409d3::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequired16eb6b572b74fc661029fcd5b2409d3($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequired16eb6b572b74fc661029fcd5b2409d3($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
